6f2c24d9ed151cd711121a1dca2228a5dc6a7d8d
thiernogueye432-jpg <thiernogueye432@gmail.com>
2026-06-28 20:43:36 +0000

    Correction finale avec redirection par dossier public

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.eaufaya.app',
   appName: 'Eau Faya',
-  webDir: '.next', // On remet .next ici
+  webDir: 'public', 
   server: {
     url: 'https://votre-site-eau-faya.com', // 👈 Remplacez par le vrai lien de votre site internet
     cleartext: true
